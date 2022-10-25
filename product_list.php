868e2b2700ba6f5cf8676da6c832f7ebc08508a8
Jacqueline-Yeh <105147490+Jacqueline-Yeh@users.noreply.github.com>
2022-10-24 21:16:43 -0700

    Add files via upload

---
+++ product_list.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Store Product List</title>
+	<? include "parts/meta.php"; ?>
+</head>
+<body>
+	<? include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+			<h2>Product List</h2>
+			<ul>
+				<li><a href="product_item.php?id">Product 1</a></li>
+				<li><a href="product_item.php?id">Product 2</a></li>
+				<li><a href="product_item.php?id">Product 3</a></li>
+				<li><a href="product_item.php?id">Product 4</a></li>
+			</ul>
+		</div>
+	</div>
+
+</body>
+</html>
