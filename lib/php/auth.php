6af22632ed0588b72361619cb0baf24f189c7a6b
Jacqueline-Yeh <105147490+Jacqueline-Yeh@users.noreply.github.com>
2022-11-15 00:23:41 -0800

    Add files via upload

---
+++ lib/php/auth.php
@@ -0,0 +1,12 @@
+<?
+
+function MYSQLIAuth() {
+	return [
+		"localhost", // localhost
+		"JaQ", // your database user name
+		"BLVCKbear0206", // database password
+		"Luxury_store", // database name
+	];
+}
+
+?>
--- product_list.php
+++ product_list.php
@@ -10,12 +10,24 @@
 	<div class="container">
 		<div class="card soft">
 			<h2>Product List</h2>
-			<ul>
-				<li><a href="product_item.php?id">Product 1</a></li>
-				<li><a href="product_item.php?id">Product 2</a></li>
-				<li><a href="product_item.php?id">Product 3</a></li>
-				<li><a href="product_item.php?id">Product 4</a></li>
-			</ul>
+			
+			<?
+				include_once "lib/php/functions.php";
+				include_once "parts/templates.php";
+
+				$result = makeQuery(
+					makeConn(),
+					"SELECT *
+					FFOM `products`
+					ORDER BY `date_create`
+					LIMIT 12
+					");
+
+				echo "<div class='productlist grid gap'>", array_reduce($result, '
+					productListTemplate'),"</div>";
+
+
+			?>
 		</div>
 	</div>
 
